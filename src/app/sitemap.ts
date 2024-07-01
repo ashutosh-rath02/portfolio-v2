628cfc298e36aa10203b4983c00c7e8bdeeeeeb1
ashutosh-rath02 <ashutosh123rath@gmail.com>
2024-07-01 12:29:44 +0530

    feat: add sitemap and robots

---
+++ src/app/sitemap.ts
@@ -0,0 +1,37 @@
+import { MetadataRoute } from "next";
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  return [
+    {
+      url: "https://ashutoshrath.me/",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 1,
+    },
+    {
+      url: `${process.env.NEXT_PUBLIC_BASE_URL}/blog`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+    },
+    {
+      url: `${process.env.NEXT_PUBLIC_BASE_URL}/projects`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+    },
+    {
+      url: `${process.env.NEXT_PUBLIC_BASE_URL}/blog/memoizatoion`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+    },
+    {
+      url: `${process.env.NEXT_PUBLIC_BASE_URL}/blog/api-fetching`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+    },
+    {
+      url: `${process.env.NEXT_PUBLIC_BASE_URL}/blog/devtools`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+    },
+  ];
+}
