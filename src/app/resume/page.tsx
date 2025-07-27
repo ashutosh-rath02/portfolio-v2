23db43ed2bff2574768205c370485427e3473887
ashutosh-rath02 <ashutosh123rath@gmail.com>
2025-07-27 20:21:09 +0530

    chore: update resume

---
+++ src/app/resume/page.tsx
@@ -0,0 +1,5 @@
+import { redirect } from "next/navigation";
+
+export default function ResumePage() {
+  redirect("/Ashutosh_Rath_SWE_1yr.pdf");
+}
--- src/components/HireMeButton.tsx
+++ src/components/HireMeButton.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const HireMeButton = () => {
   return (
-    <Link href="/Ashutosh_SWE.pdf" target="_blank">
+    <Link href="/resume" target="_blank">
       <button className="p-[2px] relative">
         <div className="absolute inset-0 bg-gradient-to-r from-indigo-500 to-purple-500 rounded-lg" />
         <div className="px-4 py-2 bg-[#020817] rounded-[6px] relative group transition duration-200 text-white hover:bg-transparent">
