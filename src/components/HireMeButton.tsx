808b0c49be6958999b5f8d13f6fe1c4d9ebcced9
ashutosh-rath02 <ashutosh123rath@gmail.com>
2025-07-30 23:03:15 +0530

    update

--- src/components/HireMeButton.tsx
+++ src/components/HireMeButton.tsx
@@ -7,7 +7,7 @@ const HireMeButton = () => {
       <button className="p-[2px] relative">
         <div className="absolute inset-0 bg-gradient-to-r from-indigo-500 to-purple-500 rounded-lg" />
         <div className="px-4 py-2 bg-[#020817] rounded-[6px] relative group transition duration-200 text-white hover:bg-transparent">
-          Hire Me
+          Hire Me!
         </div>
       </button>
     </Link>
